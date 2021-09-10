639a05520a713e1cd325510adc24548b139d323a
jonathanryukk <h15cdp@gmail.com>
2021-09-10 10:12:09 +0900

    210910

---
+++ beakjoon/1978.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+arr = list(map(int, input().split()))
+result = 0
+
+for i in arr:
+    cnt = 0
+    if i == 1:
+        continue
+    for j in range(2, i + 1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 1:
+        result += 1
+
+print(result)
