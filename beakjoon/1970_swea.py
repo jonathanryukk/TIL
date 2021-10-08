faa113220f39706daac6b5be6445e30ff6febab9
jonathanryukk <h15cdp@gmail.com>
2021-10-09 02:04:57 +0900

    211009

--- beakjoon/1970_swea.py
+++ beakjoon/1970_swea.py
@@ -1,15 +1,14 @@
-list1 = [50000,10000,5000,1000,500,100,50,10]
+change = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
 
 t = int(input())
+for tc in range(1, t + 1):
+    cnt = [0, 0, 0, 0, 0, 0, 0, 0]
+    N = int(input())
 
-for num in range(1,t+1):
-    list2 = []
-    money = int(input())
-    for i in range(len(list1)):
-        cnt = 0    
-        cnt = money//list1[i] 
-        list2.append(str(cnt))
-        money = money%list1[i]
-   
-    print(f'#{num}')
-    print(' '.join(list2))
+    for i in range(8):
+        if N // change[i] != 0:
+            cnt[i] = N // change[i]
+            N = N % change[i]
+
+    print(f'#{tc}')
+    print(*cnt)
