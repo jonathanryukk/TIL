8eef6e91097df40342ace421e4534866a0ee08d7
jonathanryukk <h15cdp@gmail.com>
2021-10-19 00:20:19 +0900

    211019

---
+++ beakjoon/2231.py
@@ -0,0 +1,10 @@
+n = int(input())
+result = 0
+for i in range(1,n + 1):
+    a = list(map(int, str(i)))
+    result = i + sum(a)
+    if result == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
