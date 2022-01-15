738e291c20f7f35f65d466af5bec0c37e6ffd01a
jonathanryukk <h15cdp@gmail.com>
2022-01-16 04:13:07 +0900

    220116

--- beakjoon/2490.py
+++ beakjoon/2490.py
@@ -1,1 +1,21 @@
-print(ord('a')-1)
+for i in range(3):
+    str = input().split()
+    cnt = 0
+    for i in range(4):
+        if str[i] == '0':
+            cnt += 1
+
+    if cnt == 0:
+        print('E')
+
+    elif cnt == 1:
+        print('A')
+
+    elif cnt == 2:
+        print('B')
+
+    elif cnt == 3:
+        print('C')
+
+    else:
+        print('D')
